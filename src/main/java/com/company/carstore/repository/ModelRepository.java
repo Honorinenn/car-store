edd1a276a831909dd3d59ee3f2b741c569be5ac2
Honorine Ndom <honorinendzah@gmail.com>
2022-10-08 18:59:05 -0700

    added the model and year repositories

--- src/main/java/com/company/carstore/repository/ModelRepository.java
+++ src/main/java/com/company/carstore/repository/ModelRepository.java
@@ -1,4 +1,12 @@
 package com.company.carstore.repository;
 
-public interface ModelRepository {
+import com.company.carstore.model.Model;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ModelRepository extends JpaRepository<Model, Integer> {
 }
+
+
+
--- src/main/java/com/company/carstore/repository/YearRepository.java
+++ src/main/java/com/company/carstore/repository/YearRepository.java
@@ -1,4 +1,13 @@
 package com.company.carstore.repository;
 
-public interface YearRepository {
+
+import com.company.carstore.model.Year;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface YearRepository extends JpaRepository<Year, Integer> {
 }
+
+
+
