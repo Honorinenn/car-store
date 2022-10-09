93b4b3c22da6a8582985df957fad7d9e72cc71c7
Honorine Ndom <honorinendzah@gmail.com>
2022-10-09 06:13:18 -0700

    added getters and setters car model

--- src/main/java/com/company/carstore/model/Car.java
+++ src/main/java/com/company/carstore/model/Car.java
@@ -13,6 +13,7 @@ import java.io.Serializable;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.HashSet;
+import java.util.Objects;
 import java.util.Set;
 
 @Entity
@@ -38,6 +39,105 @@ public class Car implements Serializable {
     private int yearId;
     private BigDecimal listPrice;
 
+    public Car(int id, Set<Model> models, String title, int brandId, LocalDate releaseDate, int yearId, BigDecimal listPrice) {
+        this.id = id;
+        this.models = models;
+        this.title = title;
+        this.brandId = brandId;
+        this.releaseDate = releaseDate;
+        this.yearId = yearId;
+        this.listPrice = listPrice;
+    }
+
+    public Car(Set<Model> models, String title, int brandId, LocalDate releaseDate, int yearId, BigDecimal listPrice) {
+        this.models = models;
+        this.title = title;
+        this.brandId = brandId;
+        this.releaseDate = releaseDate;
+        this.yearId = yearId;
+        this.listPrice = listPrice;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Set<Model> getModels() {
+        return models;
+    }
+
+    public void setModels(Set<Model> models) {
+        this.models = models;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getBrandId() {
+        return brandId;
+    }
+
+    public void setBrandId(int brandId) {
+        this.brandId = brandId;
+    }
+
+    public LocalDate getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(LocalDate releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public int getYearId() {
+        return yearId;
+    }
+
+    public void setYearId(int yearId) {
+        this.yearId = yearId;
+    }
+
+    public BigDecimal getListPrice() {
+        return listPrice;
+    }
+
+    public void setListPrice(BigDecimal listPrice) {
+        this.listPrice = listPrice;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Car car = (Car) o;
+        return id == car.id && brandId == car.brandId && yearId == car.yearId && Objects.equals(models, car.models) && Objects.equals(title, car.title) && Objects.equals(releaseDate, car.releaseDate) && Objects.equals(listPrice, car.listPrice);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, models, title, brandId, releaseDate, yearId, listPrice);
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "id=" + id +
+                ", models=" + models +
+                ", title='" + title + '\'' +
+                ", brandId=" + brandId +
+                ", releaseDate=" + releaseDate +
+                ", yearId=" + yearId +
+                ", listPrice=" + listPrice +
+                '}';
+    }
 }
 
-   
