f10f190ac635b54b99c77dd29fad647f98dd841a
Honorine Ndom <honorinendzah@gmail.com>
2022-10-09 07:15:51 -0700

    added annotations and assigned variables

--- src/main/java/com/company/carstore/model/Model.java
+++ src/main/java/com/company/carstore/model/Model.java
@@ -1,4 +1,28 @@
 package com.company.carstore.model;
 
-public class Model {
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+@Table(name = "model")
+public class Model implements Serializable {
+
+    @Id
+    @Column(name = "model_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.EAGER)
+    @JoinColumn(name = "labelId")
+    private Set<Brand> brands = new HashSet<>();
+
+    private String name;
+    private String series;
+    
 }
