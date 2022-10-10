4ceedcbfd355f3b8429db07f33c975a3c233def7
Honorine Ndom <honorinendzah@gmail.com>
2022-10-10 05:01:04 -0700

    added annotations for the year model

--- src/main/java/com/company/carstore/model/Year.java
+++ src/main/java/com/company/carstore/model/Year.java
@@ -1,4 +1,22 @@
 package com.company.carstore.model;
 
-public class Year {
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+@Table(name = "year")
+
+public class Year implements Serializable {
+
+    @Id
+    @Column(name = "year_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String title;
+    private String series;
+
+
 }
