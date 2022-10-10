0cd5f5d2e021dcc677d1a8466329b3e02ee47f1c
Honorine Ndom <honorinendzah@gmail.com>
2022-10-10 05:07:56 -0700

    added constructors, getters and setters

--- src/main/java/com/company/carstore/model/Year.java
+++ src/main/java/com/company/carstore/model/Year.java
@@ -18,5 +18,42 @@ public class Year implements Serializable {
     private String name;
     private String series;
 
+    public Year(int id, String name, String series) {
+        this.id = id;
+        this.name = name;
+        this.series = series;
+    }
+
+    public Year(String name, String series) {
+        this.name = name;
+        this.series = series;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSeries() {
+        return series;
+    }
+
+    public void setSeries(String series) {
+        this.series = series;
+    }
+
+    
+
 
 }
