39b13a1fe3e9383b097df52618746937b8be601f
Honorine Ndom <honorinendzah@gmail.com>
2022-10-12 04:02:36 -0700

    updated cartyperepository

--- src/main/java/com/company/carstore/repository/CarTypeRepository.java
+++ src/main/java/com/company/carstore/repository/CarTypeRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CarRepository extends JpaRepository<Brand, Integer> {
+public interface CarTypeRepository extends JpaRepository<Brand, Integer> {
 }
 
 
