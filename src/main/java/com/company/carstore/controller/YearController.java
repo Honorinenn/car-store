1212749f2865f7c227bc0daca1a30abd28870bea
Honorine Ndom <honorinendzah@gmail.com>
2022-10-15 23:38:34 -0700

    created controllers

--- src/main/java/com/company/carstore/controller/YearController.java
+++ src/main/java/com/company/carstore/controller/YearController.java
@@ -1,4 +1,13 @@
 package com.company.carstore.controller;
 
+
+import com.company.carstore.service.ServiceLayer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class YearController {
+
+    @Autowired
+    private ServiceLayer serviceLayer;
 }
