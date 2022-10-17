54d89d7cc080c1a5ad4de31b1f2e23974b50c8d2
Honorine Ndom <honorinendzah@gmail.com>
2022-10-17 07:36:29 -0700

    added getters and setters for viewmodel

--- src/main/java/com/company/carstore/viewmodel/BrandViewModel.java
+++ src/main/java/com/company/carstore/viewmodel/BrandViewModel.java
@@ -12,6 +12,7 @@ import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class BrandViewModel {
     private int id;
@@ -33,5 +34,96 @@ public class BrandViewModel {
         this.yearId = yearId;
         this.listPrice = listPrice;
         this.designs = designs;
+    }
+
+    public BrandViewModel(String title, int carTypeId, LocalDate releaseDate, int yearId, BigDecimal listPrice, List<Design> designs) {
+        this.title = title;
+        this.carTypeId = carTypeId;
+        this.releaseDate = releaseDate;
+        this.yearId = yearId;
+        this.listPrice = listPrice;
+        this.designs = designs;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getCarTypeId() {
+        return carTypeId;
+    }
+
+    public void setCarTypeId(int carTypeId) {
+        this.carTypeId = carTypeId;
+    }
+
+    public LocalDate getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(LocalDate releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public int getYearId() {
+        return yearId;
+    }
+
+    public void setYearId(int yearId) {
+        this.yearId = yearId;
+    }
+
+    public BigDecimal getListPrice() {
+        return listPrice;
+    }
+
+    public void setListPrice(BigDecimal listPrice) {
+        this.listPrice = listPrice;
+    }
+
+    public List<Design> getDesigns() {
+        return designs;
+    }
+
+    public void setDesigns(List<Design> designs) {
+        this.designs = designs;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        BrandViewModel that = (BrandViewModel) o;
+        return id == that.id && carTypeId == that.carTypeId && yearId == that.yearId && Objects.equals(title, that.title) && Objects.equals(releaseDate, that.releaseDate) && Objects.equals(listPrice, that.listPrice) && Objects.equals(designs, that.designs);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, title, carTypeId, releaseDate, yearId, listPrice, designs);
+    }
+
+    @Override
+    public String toString() {
+        return "BrandViewModel{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", carTypeId=" + carTypeId +
+                ", releaseDate=" + releaseDate +
+                ", yearId=" + yearId +
+                ", listPrice=" + listPrice +
+                ", designs=" + designs +
+                '}';
     }
 }
