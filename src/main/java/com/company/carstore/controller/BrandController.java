ddbb3e70ea1e687ce610f7f1ae88015913399eb0
Honorine Ndom <honorinendzah@gmail.com>
2022-10-19 12:38:30 -0700

    added the post method in the BrandController class

--- src/main/java/com/company/carstore/controller/BrandController.java
+++ src/main/java/com/company/carstore/controller/BrandController.java
@@ -4,10 +4,7 @@ import com.company.carstore.service.ServiceLayer;
 import com.company.carstore.viewmodel.BrandViewModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -23,5 +20,10 @@ public class BrandController {
         return serviceLayer.findAllBrands();
     }
 
+    @RequestMapping(value="/car", method=RequestMethod.POST)
+    @ResponseStatus(HttpStatus.CREATED)
+    public BrandViewModel createBrand(@RequestBody BrandViewModel brandViewModel) {
+        return serviceLayer.saveBrand(brandViewModel);
+    }
 
 }
