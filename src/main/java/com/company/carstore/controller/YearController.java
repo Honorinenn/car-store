860e1c05428656a7e10520257a6d9ee41eadbecd
Honorine Ndom <honorinendzah@gmail.com>
2022-10-20 15:41:19 -0700

    added get method for the Year class

--- src/main/java/com/company/carstore/controller/YearController.java
+++ src/main/java/com/company/carstore/controller/YearController.java
@@ -1,13 +1,26 @@
 package com.company.carstore.controller;
 
 
+import com.company.carstore.model.Year;
 import com.company.carstore.service.ServiceLayer;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class YearController {
 
     @Autowired
     private ServiceLayer serviceLayer;
+
+    @RequestMapping(value="/year", method= RequestMethod.GET)
+    @ResponseStatus(HttpStatus.OK)
+    public List<Year> getAllYears() {
+        return serviceLayer.findAllYears();
+    }
 }
