5595751eb9a0c65018b38963f1c6a1b098ba9e80
Honorine Ndom <honorinendzah@gmail.com>
2022-10-20 10:51:29 -0700

    added get method by id for CarType class

--- src/main/java/com/company/carstore/controller/CarTypeController.java
+++ src/main/java/com/company/carstore/controller/CarTypeController.java
@@ -1,6 +1,7 @@
 package com.company.carstore.controller;
 
 
+import com.company.carstore.exception.NoRecordFoundException;
 import com.company.carstore.model.CarType;
 import com.company.carstore.service.ServiceLayer;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,6 +26,17 @@ public class CarTypeController {
     public CarType createCarType(@RequestBody CarType carType) {
         return serviceLayer.saveCarType(carType);
     }
+
+    @RequestMapping(value="/carType/{id}", method=RequestMethod.GET)
+    @ResponseStatus(HttpStatus.OK)
+    public CarType getCarTypeById(@PathVariable int id) {
+        CarType carType = serviceLayer.findCarType(id);
+        if (carType == null) {
+            throw new NoRecordFoundException("Artist with id " + id + " does not exist.");
+        }
+        return carType;
+    }
+
 
 
 }
