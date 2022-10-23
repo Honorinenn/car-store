f1bcb00c9be8d3531834d301582d64beb848ba2b
Honorine Ndom <honorinendzah@gmail.com>
2022-10-22 20:34:28 -0700

    added annotation in the YearController

--- src/test/java/com/company/carstore/controller/YearControllerTest.java
+++ src/test/java/com/company/carstore/controller/YearControllerTest.java
@@ -1,4 +1,35 @@
 package com.company.carstore.controller;
 
+import com.company.carstore.model.Year;
+import com.company.carstore.service.ServiceLayer;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(YearController.class)
 public class YearControllerTest {
+
+    @MockBean
+    ServiceLayer serviceLayer;
+
+    private Year inputYear;
+    private Year outputYear;
+    private String inputYearString;
+    private String outputYearString;
+
+    private List<Year> allYears;
+    private String allYearsString;
+    private int yearId = 14;
+    private int nonExistentYearId = 511;
+
+    private ObjectMapper mapper = new ObjectMapper();
+
+    
+
+
 }
