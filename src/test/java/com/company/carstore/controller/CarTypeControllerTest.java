bbc63710a2ab014bd3ddd5bdddf93e7c83c92195
Honorine Ndom <honorinendzah@gmail.com>
2022-10-22 19:30:14 -0700

    added annotation in the CarTypeControllerTest

--- src/test/java/com/company/carstore/controller/CarTypeControllerTest.java
+++ src/test/java/com/company/carstore/controller/CarTypeControllerTest.java
@@ -3,12 +3,18 @@ package com.company.carstore.controller;
 import com.company.carstore.model.CarType;
 import com.company.carstore.service.ServiceLayer;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Before;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.Arrays;
 import java.util.List;
+
+import static org.mockito.Mockito.when;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(CarTypeController.class)
@@ -28,4 +34,24 @@ public class CarTypeControllerTest {
     private int nonExistentCarTypeId = 999;
 
     private ObjectMapper mapper = new ObjectMapper();
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Before
+    public void setUp() throws Exception {
+        inputCarType = new CarType(8, "audi", 4);
+        outputCarType = new CarType(carTypeId, "Jackson 5", "@jackson5Insta", "@jackson5Twitter");
+        inputCarTypeString = mapper.writeValueAsString(inputCarType);
+        outputCarTypeString = mapper.writeValueAsString(outputCarType);
+        allCarTypes = Arrays.asList(outputCarType);
+        allCarTypesString = mapper.writeValueAsString(allCarTypes);
+
+        when(serviceLayer.saveCarType(inputCarType)).thenReturn(outputCarType);
+        when(serviceLayer.findAllCarTypes()).thenReturn(allCarTypes);
+        when(serviceLayer.findCarType(carTypeId)).thenReturn(outputCarType);
+
+    }
+
+
 }
