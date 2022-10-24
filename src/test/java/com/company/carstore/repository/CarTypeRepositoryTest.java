3eb5322e78c7a22443b623a56cbf1aa9895176ae
Honorine Ndom <honorinendzah@gmail.com>
2022-10-24 08:14:53 -0700

    autowired CarTypeRepositoryTest

--- src/test/java/com/company/carstore/repository/CarTypeRepositoryTest.java
+++ src/test/java/com/company/carstore/repository/CarTypeRepositoryTest.java
@@ -1,4 +1,24 @@
 package com.company.carstore.repository;
 
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest
 public class CarTypeRepositoryTest {
+
+    @Autowired
+    CarTypeRepository carTypeRepository;
+
+    @Before
+    public void setUp() throws Exception {
+        carTypeRepository.deleteAll();
+    }
+
+    
+
+
 }
