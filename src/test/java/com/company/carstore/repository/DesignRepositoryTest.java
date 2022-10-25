9ee3f5e9dfaa45b114222ed4863c9a9d72e5f79b
Honorine Ndom <honorinendzah@gmail.com>
2022-10-25 07:27:27 -0700

    autowired DesignRepositoryTest

--- src/test/java/com/company/carstore/repository/DesignRepositoryTest.java
+++ src/test/java/com/company/carstore/repository/DesignRepositoryTest.java
@@ -1,4 +1,28 @@
 package com.company.carstore.repository;
 
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest
 public class DesignRepositoryTest {
+    @Autowired
+    DesignRepository designRepository;
+    @Autowired
+    BrandRepository brandRepository;
+    @Autowired
+    CarTypeRepository carTypeRepository;
+    @Autowired
+    YearRepository yearRepository;
+
+    @Before
+    public void setUp() throws Exception {
+        designRepository.deleteAll();
+        brandRepository.deleteAll();
+        carTypeRepository.deleteAll();
+        yearRepository.deleteAll();
+    }
 }
